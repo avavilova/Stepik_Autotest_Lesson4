8d06d950c868f3fab50bbe9a6fa9a2541ea63196
Arina Vavilova <arinavavilova29@gmail.com>
2023-02-26 20:59:00 +0300

    Redirection to login page was moved in base_page

--- pages/locators.py
+++ pages/locators.py
@@ -15,3 +15,7 @@ class ProductPageLocators():
     PRODUCT_COST_MSG = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in")
     ITEM_PRICE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
     CART_PRICE_MSG = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
